efbd636c73cc75ce5097ddbdf492b35c94936ffc
Rebellious83 <rafal.templar@gmail.com>
2022-05-04 14:51:04 +0200

    hls enabled

---
+++ Peephole.Android/MainActivity.cs
@@ -0,0 +1,44 @@
+﻿
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using MediaManager;
+using Peephole.HLS;
+
+namespace Peephole.Droid
+{
+    [Activity(Label = "Peephole", Icon = "@mipmap/icon", LaunchMode = LaunchMode.SingleTop, Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            CrossMediaManager.Current.Init(this);
+            LoadApplication(new App());
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        /// <summary>
+        /// If you want to handle when the app is opened via a notification tap, you can override OnNewIntent on your activity:
+        /// </summary>
+        /// <param name="intent"></param>
+        protected override void OnNewIntent(Intent intent)
+        {
+            base.OnNewIntent(intent);
+            // TODO: Handle app opened from notification tap.
+        }
+    }
+}
